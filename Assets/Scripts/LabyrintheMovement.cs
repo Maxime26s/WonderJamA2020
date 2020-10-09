748e58fcee4238898729ae9a18dd916dcf8d2ebf
Nicolasx08 <nic.pageau@hotmail.com>
2020-10-09 19:36:06 -0400

    2 minigames
    Manque les dessins

---
+++ Assets/Scripts/LabyrintheMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrintheMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public float playerSpeed = 2.0f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = moveInput.normalized * playerSpeed;
+    }
+    public void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+   
+}
---
+++ Assets/Scripts/SpamMiniGame.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class SpamMiniGame : MonoBehaviour
+{
+    public int n = 0;
+    public TextMeshProUGUI text;
+    private void Start()
+    {
+         n = Random.Range(20, 45);
+        text.text = "Appuyez " + n + " fois sur le boutton A pour ouvrir le coffre."; 
+    }
+    public void Update()
+    {
+         if (Input.GetButtonDown("ButtonA"))
+        {
+            if (n > 0)
+            {
+                n--;
+                text.text = "Appuyez " + n + " fois sur le boutton A pour ouvrir le coffre.";
+            }   
+        }
+        if (n == 0)
+        {
+            //chest open
+            text.text = "GG";
+        }
+    }
+}
