554df16db5852ba19e0850fa2002bcea17d34a79
Jean-Christophe <jean0314@live.ca>
2020-10-10 00:41:26 -0400

    Intro scene pretty well made thank you yes

--- Assets/Scripts/IntroScript.cs
+++ Assets/Scripts/IntroScript.cs
@@ -8,9 +8,15 @@ public class IntroScript : MonoBehaviour
     public float fadeSpeed = 0.1f;
     public float shakeDuration = 3;
     public float redDuration = 10;
+    public float textSpeed = 1;
     public GameObject star;
     public GameObject fade;
     public GameObject earth;
+    public GameObject text;
+    public GameObject xelor;
+    public GameObject rolex;
+    public GameObject nuke;
+    public List<AudioClip> sounds;
 
     // Start is called before the first frame update
     void Start()
@@ -36,15 +42,86 @@ public class IntroScript : MonoBehaviour
 
     IEnumerator Intro()
     {
+        StartCoroutine("MoveText");
+        StartCoroutine("ShowProps");
         yield return StartCoroutine("FadeIn");
         yield return StartCoroutine("ApproachEarth");
         yield return StartCoroutine("EarthShake");
         yield return StartCoroutine("EarthRed");
     }
 
+    IEnumerator ShowProps() 
+    {
+        yield return new WaitForSeconds(12);
+        for(float i = 0; i <= 0.1f; i += 0.0005f) 
+        {
+            Color c = xelor.GetComponent<SpriteRenderer>().color;
+            c.a = i;
+            xelor.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        for(float i = 0; i <= 0.1f; i += 0.0005f) 
+        {
+            Color c = nuke.GetComponent<SpriteRenderer>().color;
+            c.a = i;
+            nuke.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        yield return new WaitForSeconds(3);
+        for(float i = 0.1f; i > 0; i -= 0.0005f) 
+        {
+            Color c = xelor.GetComponent<SpriteRenderer>().color;
+            c.a = i;
+            xelor.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        for(float i = 0.1f; i > 0; i -= 0.0005f) 
+        {
+            Color c = nuke.GetComponent<SpriteRenderer>().color;
+            c.a = i;
+            nuke.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        yield return new WaitForSeconds(5);
+        for(float i = 0; i <= 0.1f; i += 0.0005f) 
+        {
+            Color c = rolex.GetComponent<SpriteRenderer>().color;
+            c.a = i;
+            rolex.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+        
+        for(float i = earth.transform.position.y; i >= -3f; i -= 0.01f) 
+        {
+            earth.transform.position = new Vector3(0, i, 0);
+            yield return null;
+        }
+    }
+
+    IEnumerator MoveText() 
+    {
+        for(float i = -400; i <= 2000f; i += textSpeed) 
+        {
+            text.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, i);
+            yield return null;
+        }
+        StartCoroutine("FadeOut");
+    }
+
     IEnumerator FadeIn() 
     {
-        for (float ft = 1f; ft >= 0; ft -= fadeSpeed) 
+        for(float ft = 1f; ft >= 0; ft -= fadeSpeed) 
+        {
+            Color c = fade.GetComponent<SpriteRenderer>().color;
+            c.a = ft;
+            fade.GetComponent<SpriteRenderer>().color = c;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut() 
+    {
+        for(float ft = 0f; ft <= 1f; ft += fadeSpeed) 
         {
             Color c = fade.GetComponent<SpriteRenderer>().color;
             c.a = ft;
@@ -55,7 +132,7 @@ public class IntroScript : MonoBehaviour
 
     IEnumerator ApproachEarth() 
     {
-        for (float size = 1f; size <= 800f; size++) 
+        for(float size = 1f; size <= 800f; size++) 
         {
             earth.transform.localScale += new Vector3(0.001f, 0.001f, 0);
             yield return null;
@@ -65,6 +142,8 @@ public class IntroScript : MonoBehaviour
 
     public IEnumerator EarthShake()
     {
+        this.GetComponent<AudioSource>().clip = sounds[0];
+        this.GetComponent<AudioSource>().Play();
         Vector3 orignalPosition = earth.transform.position;
         float elapsed = 0f;
         
@@ -82,7 +161,7 @@ public class IntroScript : MonoBehaviour
         Vector3 orignalPosition = earth.transform.position;
         float elapsed = 0f;
         
-        while (elapsed < redDuration)
+        while(elapsed < redDuration)
         {
             earth.transform.position = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
             elapsed += Time.deltaTime;
@@ -94,5 +173,6 @@ public class IntroScript : MonoBehaviour
             yield return null;
         }
         earth.transform.position = orignalPosition;
+        this.GetComponent<AudioSource>().Stop();
     }
 }
