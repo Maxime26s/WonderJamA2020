6fa1f1f53125c0fcc18a900f8d903c8cec195c7c
Jean-Christophe <jean0314@live.ca>
2020-10-11 08:06:27 -0400

    Auto stash before merge of "jc" and "origin/Maxime"

--- Assets/Scripts/DuelManager.cs
+++ Assets/Scripts/DuelManager.cs
@@ -26,8 +26,10 @@ public class DuelManager : MonoBehaviour
     public List<Image> combo, spellChoice;
     public Sprite transparent;
     public Spell currentSpell = null;
-    public GameObject enemy;
+    public GameObject enemy, smoke;
     public AnimOnly playerAnim;
+    public List<ParticleSystem> particles;
+    public List<AudioClip> particleSounds;
 
 
     // Start is called before the first frame update
@@ -215,6 +217,28 @@ public class DuelManager : MonoBehaviour
             Type enType = enemy.GetComponent<Enemy>().type;
             Type spType = spellCasted.type;
 
+            if(spType == Type.Water)
+            {
+                Instantiate(particles[0], new Vector3(enemy.transform.position.x, enemy.transform.position.y, 10), Quaternion.identity);
+                this.GetComponent<AudioSource>().clip = particleSounds[0];
+            }
+            else if(spType == Type.Air)
+            {
+                Instantiate(particles[1], new Vector3(enemy.transform.position.x, enemy.transform.position.y, 10), Quaternion.identity);
+                this.GetComponent<AudioSource>().clip = particleSounds[1];
+            }
+            else if(spType == Type.Earth)
+            {
+                Instantiate(particles[2], new Vector3(enemy.transform.position.x, enemy.transform.position.y, 10), Quaternion.identity);
+                this.GetComponent<AudioSource>().clip = particleSounds[2];
+            }
+            else if(spType == Type.Fire)
+            {
+                Instantiate(particles[3], new Vector3(enemy.transform.position.x, enemy.transform.position.y, 10), Quaternion.identity);
+                this.GetComponent<AudioSource>().clip = particleSounds[3];
+            }
+            this.GetComponent<AudioSource>().Play();
+
             if (enType == Type.Fire && spType == Type.Water
                 || enType == Type.Water && spType == Type.Air
                 || enType == Type.Air && spType == Type.Earth
@@ -319,7 +343,20 @@ public class DuelManager : MonoBehaviour
 
     public void EndFight()
     {
+        enemy.GetComponent<SpriteRenderer>().enabled = false;
+        Instantiate(smoke, new Vector3(enemy.transform.position.x, enemy.transform.position.y, 10), Quaternion.identity);
+        this.GetComponent<AudioSource>().clip = particleSounds[4];
+        this.GetComponent<AudioSource>().Play();
+        StartCoroutine("ChangeMap");
+    }
+
+    IEnumerator ChangeMap()
+    {
+        yield return new WaitForSeconds(1f);
+
         GameManager.Instance.LoadMap();
+
+        yield return null;
     }
 
 }
